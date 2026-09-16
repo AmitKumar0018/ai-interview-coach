673c07149967d2003b667cd10b138a06953f749d
amitsen12 <current12ghff@gmail.com>
2026-09-16 09:52:40 +0530

    url fix

--- client/apiServices/axios.js
+++ client/apiServices/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-interview-coach-pi-eosin.vercel.app",
+  baseURL: "https://ai-interview-coach-pi-eosin.vercel.app/api",
   withCredentials: true,
 });
 
